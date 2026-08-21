9e87ac0194f6707b14755178d6699c4c27957a56
pulinsenz <pulinsenz123@gmail.com>
2026-08-21 21:37:10 +0800

    知识库模块完成

---
+++ java-backend/src/main/java/com/agent/rag/config/OpenApiConfig.java
@@ -0,0 +1,35 @@
+package com.agent.rag.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "BearerAuth";
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("学智汇 API")
+                        .version("1.0")
+                        .description("学智汇后端接口文档")
+                        .contact(new Contact().name("学智汇")))
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME,
+                                new SecurityScheme()
+                                        .name(SECURITY_SCHEME_NAME)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("请输入登录接口返回的 token，格式：Bearer xxx")));
+    }
+}
